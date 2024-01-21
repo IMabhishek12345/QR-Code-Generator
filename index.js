7c492d5fd110070b8491d66917e000ea611575e1
abhishek pokhriyal <abhishek.pokhriyal8@gmail.com>
2024-01-22 03:14:55 +0530

    Added a download functionality for qr image if generated otherwise blank img generated if qr is not produced

--- index.js
+++ index.js
@@ -5,15 +5,33 @@ let generateBtn=document.getElementById("Generate");
 let qrbody=document.querySelector(".qr-body");
 
 let size=sizes.value;
+function isEmpty(){
+    if (qrtext.value.length>0){
+        GenerateQR();
+    }else{
+        alert("enter a valid text");   
+     }
+}
+    
 generateBtn.addEventListener("click",(e)=>{
-  e.preventDefault();
-  GenerateQR();
+   e.preventDefault();
+   isEmpty();
 })
 sizes.addEventListener("change",(e)=>{
+   
     size=e.target.value;
-    GenerateQR();
+    isEmpty();
 })
 
+downloadBtn.addEventListener("click",()=>{
+    let img=document.querySelector(".qr-body img");
+    if (img!==null){
+        let imgAttr=img.getAttribute("src");
+        downloadBtn.setAttribute("href",imgAttr);
+    }else{
+        downloadBtn.setAttribute("href",`${document.querySelector("canvas").toDataURL()}`);
+    }
+})
 function GenerateQR(){
     qrbody.innerHTML="";
     new QRCode(qrbody, {
